d5e2f6f76465391090f7c13b0a4c860584440ce4
Bill Caffery <billythakidd04@gmail.com>
2016-12-05 02:38:35 -0500

    lessons 5 & 6 add and configure DoctrineCacheBundle

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -17,6 +17,21 @@ class GenusController extends Controller
     {
         $funFact = "Octopuses can change the color of their body in just *three-tenths* of a second!";
 
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($funFact);
+
+        if($cache->contains($key))
+        {
+            $funFact = $cache->fetch($key);
+        }
+        else
+        {
+            sleep(1);
+            $funFact = $this->get('markdown.parser')->transform($funFact);
+            $cache->save($key,$funFact);
+        }
+
+
         $funFact = $this->get('markdown.parser')->transform($funFact);
 
         return $this->render('genus/show.html.twig', array(
