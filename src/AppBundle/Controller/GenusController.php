ccb419d168c2ca6ee8828f70b4f09c0c5edfc869
Bill Caffery <billythakidd04@gmail.com>
2016-12-05 02:08:36 -0500

    part 2 lesson 2 using a service (adding markdown service)

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -1,6 +1,6 @@
 <?php
-namespace AppBundle\Controller;
 
+namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -15,9 +15,13 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $templating = $this->container->get('templating');
+        $funFact = "Octopuses can change the color of their body in just *three-tenths* of a second!";
+
+        $funFact = $this->get('markdown.parser')->transform($funFact);
+
         return $this->render('genus/show.html.twig', array(
             'name' => $genusName,
+            'funFact' => $funFact,
         ));
     }
 
@@ -25,15 +29,15 @@ class GenusController extends Controller
      * @Route("/genus/{genusName}/notes", name="genus_show_notes")
      * @Method("GET")
      */
-    public function getNotesAction(){
+    public function getNotesAction($genusName)
+    {
         $notes = [
             ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
             ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
             ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
         ];
-
         $data = [
-            'notes' => $notes,
+            'notes' => $notes
         ];
 
         return new JsonResponse($data);
