f7627e3a1a646f2fa522ad169d76cee6445e23a3
Bill Caffery <billythakidd04@gmail.com>
2016-12-14 19:36:11 -0500

    using custom service and adding twig filters

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -2,34 +2,36 @@
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Cache\Cache;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 
 class MarkdownTransformer
 {
     private $markdownParser;
+    private $cache;
 
-    public function __construct(MarkdownParserInterface $markdownParser)
+    public function __construct(MarkdownParserInterface $markdownParser,Cache $cache)
     {
+
         $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
     }
 
     public function parse($str)
     {
+        $cache = $this->cache;
 
-        /*$cache = $this->get('doctrine_cache.providers.my_markdown_cache');
-        $key = md5($funFact);
+        $key = md5($str);
 
         if($cache->contains($key))
         {
-            $funFact = $cache->fetch($key);
+            return $cache->fetch($key);
         }
-        else
-        {
-            sleep(1);
-            $funFact = $this->get('markdown.parser')->transform($funFact);
-            $cache->save($key,$funFact);
-        }*/
 
-        return $this->markdownParser->transformMarkdown($str);
+        sleep(1);
+        $str = $this->markdownParser->transformMarkdown($str);
+        $cache->save($key, $str);
+
+        return $str;
     }
 }
