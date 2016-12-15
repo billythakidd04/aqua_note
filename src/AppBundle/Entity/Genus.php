7e6bb9d617b1221dd4655df2e35792afe3d48097
Bill Caffery <billythakidd04@gmail.com>
2016-12-14 19:58:01 -0500

    finish custom services

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -111,7 +111,7 @@ class Genus
      */
     public function getFunFact()
     {
-        return $this->funFact;
+        return '**TEST** - '.$this->funFact;
     }
 
     /**
--- src/AppBundle/Twig/MarkdownExtension.php
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -4,18 +4,29 @@
 namespace AppBundle\Twig;
 
 
+use AppBundle\Service\MarkdownTransformer;
+
 class MarkdownExtension extends \Twig_Extension
 {
+    private $markdownTransformer;
+
+    public function __construct(MarkdownTransformer $markdownTransformer)
+    {
+        $this->markdownTransformer = $markdownTransformer;
+    }
+
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('markdownify', [$this, 'parseMarkdown']),
+            new \Twig_SimpleFilter('markdownify', [$this, 'parseMarkdown'], [
+                'is_safe' => ['html'],
+            ]),
         ];
     }
 
     public function parseMarkdown($str)
     {
-        return strtoupper($str);
+        return $this->markdownTransformer->parse($str);
     }
 
 
