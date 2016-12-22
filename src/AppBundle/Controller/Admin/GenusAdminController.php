2e961a225c78c1768b9da75248c7ce0cb2c0d172
Bill Caffery <billythakidd04@gmail.com>
2016-12-21 21:53:24 -0500

    form templates and translations

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Form\GenusFormType;
@@ -49,6 +50,33 @@ class GenusAdminController extends Controller
         }
 
         return $this->render('admin/genus/new.html.twig',[
+            'genusForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/genus/{id}/edit", name="admin_genus_edit")
+     */
+    public function editAction(Request $request, Genus $genus)
+    {
+        $form = $this->createForm(GenusFormType::class, $genus);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $genus = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+
+            $this->addFlash('success', 'Genus updated!');
+
+            return $this->redirectToRoute('admin_genus_list');
+        }
+
+        return $this->render('admin/genus/edit.html.twig',[
             'genusForm' => $form->createView()
         ]);
     }
